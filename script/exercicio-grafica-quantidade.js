6d7171cb1328ba868cd0551fdc1b54e631d3a689
imFernandes23 <87531519+imFernandes23@users.noreply.github.com>
2022-06-06 21:22:07 -0300

    game-bee-ver2

--- script/exercicio-grafica-quantidade.js
+++ script/exercicio-grafica-quantidade.js
@@ -4,10 +4,18 @@ function resposta(){
     const iWindow = iframe.contentWindow;
     const iDocument = iWindow.document;
     const numFlowers = iDocument.getElementById('numFlowers').innerHTML;
-    const res = iDocument.getElementById('resGame').innerHTML
+    const res = iDocument.getElementById('txt')
     const handwriten = document.getElementById('result').innerHTML;
     const test = parseInt(numFlowers)+parseInt(handwriten);
 
-    console.log(res.innerHTML = handwriten)
+    if(parseInt(handwriten) == parseInt(numFlowers)){
+        res.innerHTML = `Parabéns.<br>Visitamos ${numFlowers} flores.`
+    }else if(parseInt(handwriten) > parseInt(numFlowers)){
+        res.innerHTML = `Humm...<br>Visitamos um pouco menos que ${handwriten} flores.`
+    }else if(parseInt(handwriten) < parseInt(numFlowers)){
+        res.innerHTML = `Humm...<br>Visitamos um pouco mais que ${handwriten} flores.`
+    }else{
+        res.innerHTML = `Não entendi<br>Pode escrever de novo?`
+    }
 }
 
